e3a1f4d464068db858e8a2a5fe30ea57884a3ece
Thomas Heß <thomas.hess@rwth-aachen.de>
2013-07-16 09:17:19 +0200

    included shared headers with hard links

--- THHybridCache/THLog.h
+++ THHybridCache/THLog.h
@@ -1,1 +1,22 @@
-../../THHeaders/THLog.h
+//
+//  THLog.h
+//  THHeaders
+//
+//  Created by Thomas Heß on 15.7.13.
+//  Copyright (c) 2013 Thomas Heß. All rights reserved.
+//
+
+// DLog is displayed if DEBUG is set
+#ifdef DEBUG
+#   define DLog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+#else
+#   define DLog(...)
+#endif
+
+// ALog always displays output regardless of the DEBUG setting
+#define ALog(fmt, ...) NSLog((@"%s [Line %d] " fmt), __PRETTY_FUNCTION__, __LINE__, ##__VA_ARGS__);
+
+// Only show NSLog calls if DEBUG is set
+#ifndef DEBUG
+#   define NSLog(...) DLog(...)
+#endif
--- THHybridCache/THWeakSelf.h
+++ THHybridCache/THWeakSelf.h
@@ -1,1 +1,12 @@
-../../THHeaders/THWeakSelf.h
+//
+//  THWeakSelf.h
+//  THHeaders
+//
+//  Created by Thomas Heß on 15.7.13.
+//  Copyright (c) 2013 Thomas Heß. All rights reserved.
+//
+
+/// For when you need a weak reference of an object, example: `THWeakObject(obj) wobj = obj;`
+#define THWeakObject(o) __weak __typeof__((__typeof__(o))o)
+/// For when you need a weak reference to self, example: `THWeakSelf wself = self;`
+#define THWeakSelf THWeakObject(self)
